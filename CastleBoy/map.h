bb8607743b852a7ba3225b8ba0e9cfeee0559dbc
James Downs <james.j.downs@gmail.com>
2018-03-14 23:06:52 -0400

    Changed Map::collide x parameter from int16_t to uint16_t

--- CastleBoy/map.h
+++ CastleBoy/map.h
@@ -11,7 +11,7 @@ extern bool showBackground;
 extern Entity* boss;
 
 void init(const uint8_t* source);
-bool collide(int16_t x, int8_t y, const Box& hitbox);
+bool collide(uint16_t x, int8_t y, const Box& hitbox);
 void draw();
 }
 
